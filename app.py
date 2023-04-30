32d89e05aad7710268b4d3796db59a5d1f3f59a4
alikhanmusaev <alikhanmusaev@gmail.com>
2023-04-30 18:05:14 +0300

    x

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(comment.router)
@@ -21,21 +30,11 @@ def root():
   return "Welcome to Surtash"
 
 
-origins = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:3002'
-]
-
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=['*'],
-  allow_headers=['*']
-)
-
-
 models.Base.metadata.create_all(engine)
 
 app.mount('/images', StaticFiles(directory='images'), name='images')
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, port=8000)
